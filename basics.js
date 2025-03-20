b4c8ca98e34077e6fda3e797756891136501fb3b
andrinawatsemba <andrinawatsemba@gmail.com>
2025-03-20 23:20:21 +0300

    even odd checker and looping through an array

--- basics.js
+++ basics.js
@@ -6,3 +6,17 @@ let b = 10;
 let sum = a + b;
 console.log("Sum:", sum);
 
+//Check Even or Odd
+let number = 7;
+if (number % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+
+//  Loop through an Array
+let fruits = ["Apple", "Banana", "Mango"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
