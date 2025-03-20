c4478e8ae0d931bb58dc81fa2f1a29f2f7ec9273
andrinawatsemba <andrinawatsemba@gmail.com>
2025-03-20 23:22:15 +0300

    simple function

--- basics.js
+++ basics.js
@@ -20,3 +20,10 @@ let fruits = ["Apple", "Banana", "Mango"];
 for (let i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
 }
+
+//simple function
+function greet(name) {
+    console.log("Hello, " + name + "!");
+  }
+  greet("Kenneth");
+  
